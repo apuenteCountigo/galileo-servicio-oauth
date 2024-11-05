8ed46ea671e5053c162bb9ddc8416314e2f923ea
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-11-05 16:06:10 -0500

    mapAddress Recompilando

--- servicio-oauth/src/main/java/com/formacionbdi/springboot/app/oauth/SpringbootServicioOauthApplication.java
+++ servicio-oauth/src/main/java/com/formacionbdi/springboot/app/oauth/SpringbootServicioOauthApplication.java
@@ -33,7 +33,7 @@ public class SpringbootServicioOauthApplication implements CommandLineRunner {
 			System.out.println(passwordBCrypt);
 		}
 		System.out.println("**************************************");
-		System.out.println("OAUTH V-1.1.1 24-10-28 13:56");
+		System.out.println("OAUTH V-1.1.2 24-11-05 16:05");
 
 	}
 
